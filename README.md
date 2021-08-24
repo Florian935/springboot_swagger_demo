0e7e824bd65daa9ebdb4f767ae24edbd022c0b29
florian935 <florian.martin63000@gmail.com>
2021-08-25 00:49:08 +0200

    Feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot with Swagger (Open API)
+
+This is a demo project of how to deploy a swagger with Spring Boot application.
---
+++ src/test/java/com/florian935/openapi/OpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
